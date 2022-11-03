4b3a8bc42170b03db5b5a86edb1047cef62cfdc3
Sam Paugel <shpaugel@madisoncollege.edu>
2022-11-03 13:20:36 -0500

    Calling our service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
+
+interface QuizDisplay {
+  quizName: string;
+  quizQuestions: QuestionDisplay[];
+}
+interface QuestionDisplay {
+  questionText: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'quiz-editor';
+export class AppComponent implements OnInit {
+  
+  constructor(private quizSvc: QuizService){};
+  
+  ngOnInit() {
+    const data = this.quizSvc.loadQuizzes();
+    console.log(data);
+    this.quizzes = data;
+  }
+
+  quizzes: QuizDisplay[] = [];
 }
