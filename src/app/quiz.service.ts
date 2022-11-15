6ed5dc5d370d63a4d03a36edb9cbf9029d12cf3b
Sam Paugel <shpaugel@madisoncollege.edu>
2022-11-15 13:00:32 -0600

    Refactored any[ ] out of our service

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
 
+interface QuizFromWeb {
+	name: string;
+	questions: {
+		name: string;
+	}[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,25 +14,20 @@ export class QuizService {
 
   constructor() { }
   
-  loadQuizzes = (): any[] => {
+  loadQuizzes = (): QuizFromWeb[] => {
     
     const quizzesFromWeb = [
-	{
-      name: 'Quiz 1'
-	  , questions:[
-      	{
-        	name: 'Question 1'
-      	},
-      	{
-        	name: 'Question 2'
-        }
-      ]
-	}
-		, {
-		name: 'Quiz 2',
-		questions: []
+		{ name: 'Quiz 1'
+		, questions:[
+      		{name: 'Question 1'}
+		   ,{name: 'Question 2'}
+      	]
+		}
+		,{
+		name: 'Quiz 2'
+	  , questions: []
 		}
 	];
 	return quizzesFromWeb;
-	};
+  };
 }
