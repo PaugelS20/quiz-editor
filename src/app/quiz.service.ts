e94ba876cab7cda4d9cadcb9e6d28a2e6d89614b
Sam Paugel <shpaugel@madisoncollege.edu>
2022-11-15 13:06:29 -0600

    Di the http svc into my quiz svc

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 interface QuizFromWeb {
 	name: string;
@@ -13,7 +14,7 @@ interface QuizFromWeb {
 export class QuizService {
 
   constructor(
-	private
+	private angularHttpSvc: HttpClient
   ) { }
   
   loadQuizzes = (): QuizFromWeb[] => {
